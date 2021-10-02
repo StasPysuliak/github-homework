20e3c0734591fd0f87d5c04f445f28b28567c9c9
Stanislav <staspusuliak@gmail.com>
2021-10-02 19:50:56 +0300

    Added overloaded method "print" with two parameters

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,17 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         print(scanner.nextLine());
+        print(scanner.nextLine(), scanner.nextInt());
 
     }
 
-    public static void print (String text) {
+    public static void print(String text) {
         System.out.println(text);
+    }
+
+    public static void print(String text, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(text);
+        }
     }
 }
